8915ffa6d6b9164c443e0aba8f085d5445a00174
Joc <callmejoc@gmail.com>
2022-12-10 17:04:19 +0000

    completed section 21

--- src/pages/SignUpPage.spec.js
+++ src/pages/SignUpPage.spec.js
@@ -1,12 +1,11 @@
 import SignUpPage from "./SignUpPage.svelte";
-import { render, screen } from "@testing-library/svelte";
+import { render, screen, waitFor } from "@testing-library/svelte";
 import userEvent from "@testing-library/user-event";
 import "@testing-library/jest-dom";
 import { setupServer } from "msw/node";
 import { rest } from "msw";
 
-
-describe("Sign up page", () => {
+describe("Sign Up Page", () => {
   describe("Layout", () => {
     it("has Sign Up header", () => {
       render(SignUpPage);
@@ -33,6 +32,11 @@ describe("Sign up page", () => {
       const input = screen.getByLabelText("Password");
       expect(input.type).toBe("password");
     });
+    it("has password repeat input", () => {
+      render(SignUpPage);
+      const input = screen.getByLabelText("Password Repeat");
+      expect(input).toBeInTheDocument();
+    });
     it("has password type for password repeat input", () => {
       render(SignUpPage);
       const input = screen.getByLabelText("Password Repeat");
@@ -40,119 +44,97 @@ describe("Sign up page", () => {
     });
     it("has Sign Up button", () => {
       render(SignUpPage);
-      const header = screen.getByRole("button", { name: "Sign Up" });
-      expect(header).toBeInTheDocument();
+      const button = screen.getByRole("button", { name: "Sign Up" });
+      expect(button).toBeInTheDocument();
     });
-    it("has Sign Up button initially disabled", () => {
+    it("disables the button initially", () => {
       render(SignUpPage);
       const button = screen.getByRole("button", { name: "Sign Up" });
       expect(button).toBeDisabled();
     });
   });
-  describe("interactions", () => {
+  describe("Interactions", () => {
+    let counter = 0;
+    let requestBody;
+    const server = setupServer(
+      rest.post("/api/1.0/users", (req, res, ctx) => {
+        requestBody = req.body;
+        counter++;
+        return res(ctx.status(200), /**/ ctx.delay(50));
+      })
+    );
+
+    beforeAll(() => server.listen());
+    beforeEach(() =>{
+      counter=0;
+      // Below resets the server to the original definition above
+      // see it("does not display account activation information after failing sign up request" test for example
+      server.resetHandlers();
+    });
+    afterAll(() => server.close());
+
+
+    let button;
     const setup = async () => {
       render(SignUpPage);
       const usernameInput = screen.getByLabelText("Username");
       const emailInput = screen.getByLabelText("E-mail");
       const passwordInput = screen.getByLabelText("Password");
       const passwordRepeatInput = screen.getByLabelText("Password Repeat");
+      button = screen.getByRole("button", { name: "Sign Up" });
       await userEvent.type(usernameInput, "user1");
       await userEvent.type(emailInput, "user1@mail.com");
       await userEvent.type(passwordInput, "P4ssword");
       await userEvent.type(passwordRepeatInput, "P4ssword");
     };
 
-    it("enables button when password and password repeat match", async () => {
+    it("enables the button when the password and password repeat fields have same value", async () => {
       await setup();
-      const button = screen.getByRole("button", { name: "Sign Up" });
       expect(button).toBeEnabled();
     });
-    it("sends username, email and password to backend on buttonclick", async () => {
-      let requestBody;
-      const server = setupServer(
-        rest.post("/api/1.0/users", (req, res, ctx) => {
-          requestBody = req.body;
-          return res(ctx.status(200));
-        })
-      );
-      server.listen({
-        onUnhandledRequest: "warn",
-      });
-      await setup();
-      const button = screen.getByRole("button", { name: "Sign Up" });
-      await userEvent.click(button);
-      await server.close();
 
+    it("sends username, email and password to backend after clicking the button", async () => {
+      await setup();
+      await userEvent.click(button);
+
+      await screen.findByText("Please check your e-mail to activate your account");
       expect(requestBody).toEqual({
+        username: "user1",
         email: "user1@mail.com",
         password: "P4ssword",
-        username: "user1",
       });
     });
-
     it("disables button when there is an ongoing api call", async () => {
-      let counter = 0;
-      const server = setupServer(
-        rest.post("/api/1.0/users", (req, res, ctx) => {
-          counter++;
-          return res(ctx.status(200));
-        })
-      );
-      server.listen({
-        onUnhandledRequest: "warn",
-      });
+      server.listen();
       await setup();
-      const button = screen.getByRole("button", { name: "Sign Up" });
       await userEvent.click(button);
       await userEvent.click(button);
-
-      await server.close();
+      await screen.findByText("Please check your e-mail to activate your account");
       expect(counter).toBe(1);
     });
 
     it("displays spinner while the api request in progress", async () => {
-      const server = setupServer(
-        rest.post("/api/1.0/users", (req, res, ctx) => {
-          return res(ctx.status(200));
-        })
-      );
-
-      server.listen();
       await setup();
-      const button = screen.getByRole("button", { name: "Sign Up" });
-
       await userEvent.click(button);
-
-      const spinner = screen.queryByRole("status");
+      const spinner = screen.getByRole("status");
       expect(spinner).toBeInTheDocument();
     });
 
-    it("does not display spinner when there is no api request in progress", async () => {
+    it("does not display spinner when there is no api request", async () => {
       await setup();
-      // for details on queryByXXX vs getByXXX see https://testing-library.com/docs/dom-testing-library/cheatsheet/
       const spinner = screen.queryByRole("status");
       expect(spinner).not.toBeInTheDocument();
     });
 
-    it("display account activation information after successful sign up request", async () => {
-      const server = setupServer(
-        rest.post("/api/1.0/users", (req, res, ctx) => {
-          return res(ctx.status(200));
-        })
-      );
-      server.listen();
+    it("displays account activation information after successful sign up request", async () => {
       await setup();
-      const button = screen.getByRole("button", { name: "Sign Up" });
       await userEvent.click(button);
-      await server.close();
-
-      // using findBy... as it supports await
-      // for more details see https://testing-library.com/docs/dom-testing-library/cheatsheet/
       const text = await screen.findByText(
-        "Please check your email to activate your account"
+        "Please check your e-mail to activate your account"
       );
       expect(text).toBeInTheDocument();
     });
+
     it("does not display account activation message before sign up request", async () => {
       await setup();
       const text = screen.queryByText(
@@ -160,5 +142,32 @@ describe("Sign up page", () => {
       );
       expect(text).not.toBeInTheDocument();
     });
+
+    it("does not display account activation information after failing sign up request", async () => {
+      server.use(
+        rest.post("/api/1.0/users", (req, res, ctx) => {
+          // the res.once below means it will only use this value one time, then return to 
+          // using the default  ctx.status(400)
+          // this is an alternative to using "server.resetHandlers()" in the before each
+          return res.once(ctx.status(400));
+        })
+      ); 
+      await setup();
+      await userEvent.click(button);
+      const text = screen.queryByText(
+        "Please check your e-mail to activate your account"
+      );
+      expect(text).not.toBeInTheDocument();
+    });
+
+    it("hides sign up form after successful sign up request", async () => {
+      await setup();
+      const form = screen.getByTestId("form-sign-up");
+      await userEvent.click(button);
+      await waitFor(() => {
+        expect(form).not.toBeInTheDocument();
+      });
+    });
+
   });
 });
--- src/pages/SignUpPage.svelte
+++ src/pages/SignUpPage.svelte
@@ -1,74 +1,59 @@
 <script>
-  //import "whatwg-fetch";
-  import axios from "axios";
+  import axios from 'axios';
   let disabled = true;
   let username, email, password, passwordRepeat;
 
-  $: disabled = password && passwordRepeat ? password !== passwordRepeat : true;
+  $: disabled = (password && passwordRepeat) ? password !== passwordRepeat : true;
 
-  let apiProgress;
-  let signUpSuccess = false;
+  let apiProgress = false;
+  let signUpSucess = false;
+
   const submit = () => {
     disabled = true;
-    apiProgress = axios.post("/api/1.0/users", {username,email,password});
-    apiProgress.then(()=>{
-      signUpSuccess = true;
+    apiProgress = true;
+    axios.post('/api/1.0/users', { username, email, password }).then(() => {
+      signUpSucess = true;
+    }).catch((error) => {
+
     });
 
-    /*
-    fetch("/api/1.0/users", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ username, email, password }),
-    });
-  */
-  };
+  }
+
 </script>
-
 <div class="col-lg-6 offset-lg-3 col-md-8 offset-md-2">
-    <form class="card mt-5">
+  {#if !signUpSucess}
+    <form class="card mt-5" data-testid="form-sign-up">
       <div class="card-header">
         <h1 class="text-center">Sign Up</h1>
       </div>
       <div class="card-body">
         <div class="form-group">
           <label for="username">Username</label>
-          <input id="username" class="form-control" bind:value={username} />
+          <input id="username" class="form-control" bind:value={username}/>
         </div>
         <div class="form-group">
           <label for="e-mail">E-mail</label>
-          <input id="e-mail" class="form-control" bind:value={email} />
+          <input id="e-mail" class="form-control" bind:value={email}/>
         </div>
         <div class="form-group">
           <label for="password">Password</label>
-          <input
-            id="password"
-            type="password"
-            class="form-control"
-            bind:value={password}
-          />
+          <input id="password" type="password" class="form-control" bind:value={password}/>
         </div>
         <div class="form-group">
           <label for="password-repeat">Password Repeat</label>
-          <input
-            id="password-repeat"
-            type="password"
-            class="form-control"
-            bind:value={passwordRepeat}
-          />
+          <input id="password-repeat" type="password" class="form-control" bind:value={passwordRepeat}/>
         </div>
         <div class="text-center">
           <button class="btn btn-primary" {disabled} on:click|preventDefault={submit}>
-            {#await apiProgress}
-            <span class="spinner-border spinner-border-sm" role="status"></span>
-            {/await}
+            {#if apiProgress}
+              <span class="spinner-border spinner-border-sm" role="status"></span>
+            {/if}
+            
             Sign Up</button>
         </div>
       </div>
     </form>
-    {#if signUpSuccess}
-      <div class="alert alert-success">Please check your email to activate your account</div>
-    {/if}
+  {:else}
+    <div class="alert alert-success mt-3">Please check your e-mail to activate your account</div>
+  {/if}
 </div>
